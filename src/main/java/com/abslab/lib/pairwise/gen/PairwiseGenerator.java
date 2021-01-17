024eca602aca7e64dfd5e4a4a0958ef65d867ccf
3draven <3draven@gmail.com>
2021-01-17 14:23:03 +0100

    Added helper methods and headers to source

--- src/main/java/com/abslab/lib/pairwise/gen/PairwiseGenerator.java
+++ src/main/java/com/abslab/lib/pairwise/gen/PairwiseGenerator.java
@@ -1,15 +1,32 @@
+/*******************************************************************************
+ * Copyright 2021 Renat Eskenin
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package com.abslab.lib.pairwise.gen;
 
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.NoSuchElementException;
 import java.util.Spliterators;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -44,7 +61,23 @@ public class PairwiseGenerator<C, E> implements Iterator<List<E>> {
     /**
      * Summary generated rows count
      */
+    @Getter
     private int rowsCount;
+
+    /**
+     *
+     * @param i test case number
+     * @return Map with this test case where key is param name and value is param
+     *         value
+     */
+    public Map<C, E> getTestCase(int i) {
+        if (i >= rowsCount) {
+            throw new IndexOutOfBoundsException(String.format("We do not have this test [%d]", i));
+        }
+
+        return generatedCases.entrySet().stream()
+                .collect(Collectors.toMap(Entry<C, List<E>>::getKey, e -> e.getValue().get(i)));
+    }
 
     /**
      *
